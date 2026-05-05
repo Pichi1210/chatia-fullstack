1cd9fd5c7a3dce685836bf39fe5f13b95254a103
pichi <jonathanaltami1@gmail.com>
2026-05-05 23:36:39 +0300

    correccion de errores

--- frontend/src/routes/_layout/chat.tsx
+++ frontend/src/routes/_layout/chat.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
 import ChatInterface from "@/components/Chat/ChatInterface"
 
-export const Route = createFileRoute('/_layout/chat')({
+export const Route = createFileRoute("/_layout/chat")({
   component: ChatPage,
 })
 
